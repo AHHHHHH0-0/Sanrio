5d8dc571197d23f9d52a00f575f9c91a56743af1
Andrew Liu <landrew1018@gmail.com>
2025-06-09 23:15:57 -0700

    Initial Commit

---
+++ SanrioCharacterAppTests/SanrioCharacterAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SanrioCharacterAppTests.swift
+//  SanrioCharacterAppTests
+//
+//  Created by Andrew Liu on 6/9/25.
+//
+
+import Testing
+@testable import SanrioCharacterApp
+
+struct SanrioCharacterAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
